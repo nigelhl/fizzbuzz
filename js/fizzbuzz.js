37f1bbfb0c1e6a2d6158028396dc0cb33c041ff9
nigel <nmillward@tripadvisor.com>
2015-09-14 10:22:37 +0100

    new fitbuzz solution

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,25 +1,6 @@
 $(document).ready(function(){
 	var i;
 	for(i=1; i <=100; i++){
-		
-		// Fizz Buzz
-		if (i % 3 == 0 && i % 5 == 0){
-			$("body").append("<p>Fizz Buzz</p>");
-		}
-
-		// Fizz
-		else if(i % 3 == 0){
-			$("body").append("<p>Fizz</p>");
-		}
-
-		// Buzz
-		else if (i % 5 == 0){
-			$("body").append("<p>Buzz</p>");
-		} 	
-
-		// Numbers
-		else {
-			$("body").append("<p>" + i + "</p>");
-		}
+		$("body").append("<p>" + (i%3!=0&&i%5!=0 ? i:"") + " " + (i%3==0 ? "Fizz":"") + (i%5==0 ? "Buzz":"") + "</p>");
 	}
 });
